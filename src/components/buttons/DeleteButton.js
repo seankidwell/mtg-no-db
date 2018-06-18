1cecb83ad0f106343ffb73db704b35c276f73227
Sean Kidwell <seankidwell@Seans-MacBook-Pro.local>
2018-06-18 12:28:39 -0600

    button styling

--- src/components/buttons/DeleteButton.js
+++ src/components/buttons/DeleteButton.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function DeleteButton(props) {
   return (
-    <button onClick={props.fav ? null : props.deleteFn}>DELETE</button>
+    <button className="deleteButton" onClick={props.fav ? null : props.deleteFn}>DELETE</button>
   )
 }
